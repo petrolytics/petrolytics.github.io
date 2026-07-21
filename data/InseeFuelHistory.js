56c39fc7dd3a31d1d866410c8026152190933368
alex-ception <636722+alex-ception@users.noreply.github.com>
2026-07-21 01:01:46 +0000

    [bot] chore: auto-update daily data snapshots (DGEC & FRED)

--- data/InseeFuelHistory.js
+++ data/InseeFuelHistory.js
@@ -112,5 +112,6 @@
 //   InseeFuelHistory.2026-07-18.js
 //   InseeFuelHistory.2026-07-19.js
 //   InseeFuelHistory.2026-07-20.js
+//   InseeFuelHistory.2026-07-21.js
 
-export { FUEL_PRICE_HISTORY } from './sources/InseeFuelHistory.2026-07-20.js';
+export { FUEL_PRICE_HISTORY } from './sources/InseeFuelHistory.2026-07-21.js';
